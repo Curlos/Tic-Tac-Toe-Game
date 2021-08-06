17ff455709a6e655fb8fe191da201b4bf7f90805
Carlos Martinez <curlosmart@gmail.com>
2021-08-06 10:56:28 -0400

    markers can be placed correctly now

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const grid = document.querySelectorAll('.cell')
+const restartGameButton = document.getElementById('restartGame')
 
 const handleClick = (event) => {
-    console.log(event.target)
+    const cell = event.target
+    console.log(`CELL: `)
+    console.log(cell)
+    const marker = cell.childNodes[1]
+    console.log(isMarked(marker))
+
+    if (!isMarked(marker)) {
+        console.log("MARKED")
+        marker.textContent = 'X'
+    } else {
+        console.log('ALREADY MARKED')
+    }
+
+    
 }
+
+const restartGame = () => {
+    grid.forEach((cell) => {
+        const marker = cell.childNodes[1]
+        marker.innerHTML = '&nbsp;'
+    })
+}
+
+const isMarked = (marker) => marker.innerHTML === '&nbsp;' ? false : true
 
 for (let cell of grid) {
     cell.addEventListener('click', handleClick)
     console.log(cell)
 }
+
+restartGameButton.addEventListener('click', restartGame)
