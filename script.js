792ac1fc93d66430611aa463e93f3afc5725f74d
Carlos Martinez <curlosmart@gmail.com>
2021-08-06 11:24:33 -0400

    added players and additional marker functionality

--- script.js
+++ script.js
@@ -1,24 +1,72 @@
 const grid = document.querySelectorAll('.cell')
 const restartGameButton = document.getElementById('restartGame')
 
+const gameBoard = {
+    "1": "&nbsp;",
+    "2": "&nbsp;",
+    "3": "&nbsp;",
+    "4": "&nbsp;",
+    "5": "&nbsp;",
+    "6": "&nbsp;",
+    "7": "&nbsp;",
+    "8": "&nbsp;",
+    "9": "&nbsp;",
+}
+
+const Player = (name, marker) => {
+
+    let score = 0
+
+    const getName = () => name
+    const getScore = () => score
+    const getMarker = () => marker
+
+    const incrementScore = () => {
+        score++
+    }
+
+    return {getName, getScore, getMarker, incrementScore}
+
+}
+
 const handleClick = (event) => {
     const cell = event.target
+    console.log(cell.attributes)
+    const positionValue = cell.getAttribute('positionValue')
     console.log(`CELL: `)
-    console.log(cell)
+    console.log(positionValue)
+    console.log(`GAMEBOARD VALUE: ${gameBoard[positionValue]}`)
     const marker = cell.childNodes[1]
-    console.log(isMarked(marker))
 
     if (!isMarked(marker)) {
-        console.log("MARKED")
-        marker.textContent = 'X'
-    } else {
-        console.log('ALREADY MARKED')
+        markBox(marker, positionValue)
     }
 
     
 }
 
+const markBox = (marker, positionValue) => {
+    let markerContent;
+
+    if (currentPlayerTurn === player1) {
+        markerContent = player1.getMarker()
+        currentPlayerTurn = player2
+    } else {
+        markerContent = player2.getMarker()
+        currentPlayerTurn = player1
+    }
+
+    marker.textContent = markerContent
+    gameBoard[positionValue] = markerContent
+}
+
+const gameOver = (player1, player2) => {
+    
+}
+
 const restartGame = () => {
+    currentPlayerTurn = player1
+    
     grid.forEach((cell) => {
         const marker = cell.childNodes[1]
         marker.innerHTML = '&nbsp;'
@@ -33,3 +81,7 @@ for (let cell of grid) {
 }
 
 restartGameButton.addEventListener('click', restartGame)
+
+const player1 = Player('Player1', 'X')
+const player2 = Player('Player2', 'O')
+let currentPlayerTurn = player1
