7cd7e13157b7c90556fd48fc83729697ba7911de
Carlos Martinez <curlosmart@gmail.com>
2021-08-06 12:21:02 -0400

    all winning positions checked and game ends

--- script.js
+++ script.js
@@ -31,11 +31,8 @@ const Player = (name, marker) => {
 
 const handleClick = (event) => {
     const cell = event.target
-    console.log(cell.attributes)
     const positionValue = cell.getAttribute('positionValue')
-    console.log(`CELL: `)
     console.log(positionValue)
-    console.log(`GAMEBOARD VALUE: ${gameBoard[positionValue]}`)
     const marker = cell.childNodes[1]
 
     if (!isMarked(marker)) {
@@ -58,15 +55,40 @@ const markBox = (marker, positionValue) => {
 
     marker.textContent = markerContent
     gameBoard[positionValue] = markerContent
+    allPos[markerContent].push(Number(positionValue))
+    console.log(allPos)
+    isGameOver(markerContent)
 }
 
-const gameOver = (player1, player2) => {
-    
+const isGameOver = (markerContent) => {
+    const allWinningPosVal = Object.values(allWinningPositions)
+    console.log(allWinningPosVal)
+
+    allWinningPosVal.forEach((arr) => {
+        arr.forEach((winningPosVal) => {
+            if (allPos[markerContent].length >= 3) {
+                const checkIfAllWinningPos = allPos[markerContent].every(pos => winningPosVal.includes(pos))
+
+                if (checkIfAllWinningPos) {
+                    console.log('GAME OVER')
+                    return true
+                }
+            }
+        })
+    })
+
+    return false
+}
+
+const endGame = () => {
+
 }
 
 const restartGame = () => {
     currentPlayerTurn = player1
-    
+    allPos['X'] = []
+    allPos['O'] = []
+
     grid.forEach((cell) => {
         const marker = cell.childNodes[1]
         marker.innerHTML = '&nbsp;'
@@ -75,13 +97,31 @@ const restartGame = () => {
 
 const isMarked = (marker) => marker.innerHTML === '&nbsp;' ? false : true
 
-for (let cell of grid) {
-    cell.addEventListener('click', handleClick)
-    console.log(cell)
+const startGame = () => {
+    for (let cell of grid) {
+        cell.addEventListener('click', handleClick)
+        console.log(cell)
+    }
+
+    restartGame()
+    
+    restartGameButton.addEventListener('click', restartGame)
+
+    console.log(allWinningPositions)
 }
 
-restartGameButton.addEventListener('click', restartGame)
+const allWinningPositions = {
+    "horizontalLine": [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    "verticalLine": [[1, 5, 9], [3, 5, 7], [3, 6, 9]],
+    "diagonalLine": [[1, 4, 7], [2, 5, 8]]
+}
 
+const allPos = {
+    'X': [],
+    'O': []
+}
 const player1 = Player('Player1', 'X')
 const player2 = Player('Player2', 'O')
 let currentPlayerTurn = player1
+
+startGame()
